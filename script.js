02318ddd59d4e0200fad2968c25a37aeb44d13dd
Abu <sufianmondal.eee@gmail.com>
2023-08-25 15:23:42 +0600

    ternary operator basic

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-num1 = prompt("Enter frist Number : ");
-num2 = prompt("Enter second Number: ");
+// num1 = prompt("Enter frist Number : ");
+// num2 = prompt("Enter second Number: ");
 
-num1 = parseInt(num1, 10);
-num2 = parseInt(num2, 10);
+// num1 = parseInt(num1, 10);
+// num2 = parseInt(num2, 10);
 
-var sum, sub, mul, div, rem;
+// var sum, sub, mul, div, rem;
 
-sum = num1 + num2;
-console.log(num1 + "+" + num2 + "=" + sum);
+// sum = num1 + num2;
+// console.log(num1 + "+" + num2 + "=" + sum);
 
-sub = num1 - num2;
-console.log(num1 + "-" + num2 + "=" + sub);
+// sub = num1 - num2;
+// console.log(num1 + "-" + num2 + "=" + sub);
 
-mul = num1 * num2;
-console.log(num1 + "*" + num2 + "=" + mul);
+// mul = num1 * num2;
+// console.log(num1 + "*" + num2 + "=" + mul);
 
-div = num1 / num2;
-console.log(num1 + "/" + num2 + "=" + div);
+// div = num1 / num2;
+// console.log(num1 + "/" + num2 + "=" + div);
 
-rem = num1 / num2;
-console.log(num1 + "%" + num2 + "=" + rem);
+// rem = num1 / num2;
+// console.log(num1 + "%" + num2 + "=" + rem);
+
+var num1 = Number(prompt("Enter Your First Number"));
+var num2 = Number(prompt("Enter Your Second Number"));
+var num3 = Number(prompt("Enter Your Third Number"));
+
+var result =
+  num1 > num2 && num1 > num3 ? num1 : num2 > num1 && num2 > num3 ? num2 : num3;
+
+console.log(result);
