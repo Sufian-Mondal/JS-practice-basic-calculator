df18f8a3a853230e5d143e1551d8a16603352094
Abu <sufianmondal.eee@gmail.com>
2023-08-26 11:46:58 +0600

    Array

--- script.js
+++ script.js
@@ -21,11 +21,29 @@
 // rem = num1 / num2;
 // console.log(num1 + "%" + num2 + "=" + rem);
 
-var num1 = Number(prompt("Enter Your First Number"));
-var num2 = Number(prompt("Enter Your Second Number"));
-var num3 = Number(prompt("Enter Your Third Number"));
+// var num1 = Number(prompt("Enter Your First Number"));
+// var num2 = Number(prompt("Enter Your Second Number"));
+// var num3 = Number(prompt("Enter Your Third Number"));
 
-var result =
-  num1 > num2 && num1 > num3 ? num1 : num2 > num1 && num2 > num3 ? num2 : num3;
+// var result =
+//   num1 > num2 && num1 > num3 ? num1 : num2 > num1 && num2 > num3 ? num2 : num3;
 
-console.log(result);
+// console.log(result);
+
+//using IIFEs function
+
+//
+
+function highestScore(scores) {
+  let max = scores[0];
+  for (let x = 1; x < scores.length; x++) {
+    if (max < scores[x]) {
+      max = scores[x];
+    }
+  }
+  return max;
+}
+
+let scores = [21, 28, 2, 88, 15];
+let maxScore = highestScore(scores);
+console.log(maxScore);
